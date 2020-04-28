4cf132a30ecba27c9a344058bfe91485abbe5994
Alexander <alexanderj2357@gmail.com>
2020-04-29 08:09:39 +1000

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,7 +7,7 @@ import * as firebaseui from "firebaseui";
 import { message } from "antd";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPycHenv2ptI8SCfkak_5wlJoje0JNbRM",
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   authDomain: "baobab-82803.firebaseapp.com",
   databaseURL: "https://baobab-82803.firebaseio.com",
   projectId: "baobab-82803",
